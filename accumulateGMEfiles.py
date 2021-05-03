8e0e5fe262582487fc8ffa87c9ad4bc75c7d8e77
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-03 18:35:54 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -12,18 +12,30 @@ def pullNasdaqAbbreves():
     ActivendqAdd=mglob.find("ActivendqAbbrev");
     if ActivendqAdd>-1:
        os.system('rm ActivendqAbbrev');
-       
+    
+    nasdaqNativeAbbreviations="http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt"   
     nasdaqAbbreviations="http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
     filedate=str(datetime.now().date())
+    
+    ndqNativeAbbrecords=str('curl '+nasdaqNativeAbbreviations+' -o nasdaqNativeAbbreviations-'+filedate)
+    ActNativendqAbbrv=str('curl '+nasdaqNativeAbbreviations+' -o ActiveNativendqAbbrev')
+    
     ndqAbbrecords=str('curl '+nasdaqAbbreviations+' -o nasdaqAbbreviations-'+filedate)
     ActndqAbbrv=str('curl '+nasdaqAbbreviations+' -o ActivendqAbbrev')
+    
+    os.system(ndqNativeAbbrecords)
+    os.system(ActNativendqAbbrv)
     os.system(ndqAbbrecords)
     os.system(ActndqAbbrv)
+    
     print("accumilateGMEfiles ran")
     print("accumilateGMEfiles ran")
     
 pullNasdaqAbbreves();
+NasdaqNativeAbbreviations=pandas.read_csv('ActivendqAbbrev','|');
 NasdaqAbbreviations=pandas.read_csv('ActivendqAbbrev','|');
+
 print(NasdaqAbbreviations)
+print(NasdaqNativeAbbreviations)
 
 
