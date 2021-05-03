84a6db5769c2543f10da670b152eb94c17748ce1
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-03 14:52:56 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -1,13 +1,14 @@
 import os
-
 from datetime import datetime
 
-
-nasdaqAbbreviations="http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
-print(nasdaqAbbreviations)
-filedate=str(datetime.now().date())
-ndq=str('curl '+nasdaqAbbreviations+' -o nasdaqAbbreviations-'+filedate)
-print(ndq)
-os.system(ndq)
-print("accumilateGMEfiles ran")
-print("accumilateGMEfiles ran")
+def pullNasdaqAbbreves():
+    nasdaqAbbreviations="http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
+    print(nasdaqAbbreviations)
+    filedate=str(datetime.now().date())
+    ndq=str('curl '+nasdaqAbbreviations+' -o nasdaqAbbreviations-'+filedate)
+    print(ndq)
+    os.system(ndq)
+    print("accumilateGMEfiles ran")
+    print("accumilateGMEfiles ran")
+    
+#pullNasdaqAbbreves()
