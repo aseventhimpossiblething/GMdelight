766a7e7b08705dc0e47e0d5aabe670db10db0b40
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-03 15:12:26 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -1,14 +1,24 @@
 import os
 from datetime import datetime
+import pandas
+
+print(os.cwd())
+print(os.listdir())
 
 def pullNasdaqAbbreves():
     nasdaqAbbreviations="http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
     print(nasdaqAbbreviations)
     filedate=str(datetime.now().date())
-    ndq=str('curl '+nasdaqAbbreviations+' -o nasdaqAbbreviations-'+filedate)
-    print(ndq)
-    os.system(ndq)
+    ndqAbbrecords=str('curl '+nasdaqAbbreviations+' -o Sheets/nasdaqAbbreviations-'+filedate)
+    ActndqAbbrv=str('curl '+nasdaqAbbreviations+' -o Sheets/ActivendqAbbrev')
+    @print(ndq)
+    os.system(ndqAbbrecords)
+    os.system(ActndqAbbrv)
     print("accumilateGMEfiles ran")
     print("accumilateGMEfiles ran")
     
-pullNasdaqAbbreves()
+pullNasdaqAbbreves();
+print(os.listdir());
+#os.chdir();
+
+
