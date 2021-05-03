8edb184ae070776c45c2b87e35cf82415c1d657b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-03 17:07:15 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -8,22 +8,22 @@ print(os.listdir())
 
 def pullNasdaqAbbreves():
     os.chdir('Sheets');
-    print(os.listdir());
+    #print(os.listdir());
     mglob=str(os.listdir());
     findit=mglob.find("BidOp")
-    print(findit)
-    print("glob Data Type "+str(type(mglob)))
+    #print(findit)
+    #print("glob Data Type "+str(type(mglob)))
     #os.system('rm ActivendqAbbrev')
-    print(os.listdir());
+    #print(os.listdir());
     nasdaqAbbreviations="http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
-    print(nasdaqAbbreviations)
+    #print(nasdaqAbbreviations)
     filedate=str(datetime.now().date())
     ndqAbbrecords=str('curl '+nasdaqAbbreviations+' -o nasdaqAbbreviations-'+filedate)
     #ActndqAbbrv=str('curl '+nasdaqAbbreviations+' -o ActivendqAbbrev')
     #print(ndq)
     #os.system(ndqAbbrecords)
     #os.system(ActndqAbbrv)
-    print("ac#cumilateGMEfiles ran")
+    print("accumilateGMEfiles ran")
     print("accumilateGMEfiles ran")
     
 pullNasdaqAbbreves();
