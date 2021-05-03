65fad559dc9c3b44d6b98a935f8f5e2085932ac3
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-03 09:53:11 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -9,29 +9,17 @@ pwd="open"
 
 
 
-print("1.1")
-import glob
-print("1.2")
-import numpy
-print("1.3")
-import scipy
-print("1.4")
-import pandas
-print("1.5")
-import BidOpAssist
-print("1.6")
-print("1.7")
-import fileHandler
-print("1.8")
-import os
-print("1.9")
-from flask import Flask, Markup, render_template, request, make_response
-print("1.11")
-from flask import send_file
-print("1.12")
-from flask import send_from_directory
-print("1.13")
 
+import glob
+import numpy
+import scipy
+import pandas
+import BidOpAssist
+import fileHandler
+import os
+from flask import Flask, Markup, render_template, request, make_response
+from flask import send_file
+from flask import send_from_directory
 
 
 
@@ -45,7 +33,6 @@ os.system('sudo chmod -R 777 Sheets')
 os.system('sudo chmod -R 777 templates')
 
 
-print("3")
 
 
 import psycopg2
@@ -68,7 +55,7 @@ def logonfalse():
     return "no pass"
 
 
-print("4")
+
 
 
 def chckbdxcred():
@@ -124,22 +111,7 @@ print("6")
 
 
 
-"""
-@app.route('/apiresponse')
-def off1():
-    print("api response called")    
-    return "api response called"      
-     
 
-
-         
-@app.route('/get_campaigns')
-def official():
-    get_campaigns     
-    return "."      
-       
-         
-"""
 @app.route('/favicon.png')
 def favicon():
     return send_from_directory('/app/favicon.png','favicon')     
@@ -149,199 +121,6 @@ def favicon():
 print("7")
 
 """
-@app.route('/DisplayCommUpdate')
-def CommUpdateDisplay():
-    os.chdir('/var/www/workPortal/Sheets/')     
-    storeRequest=open('RequestsVsResponses.txt','r+')
-    read_storeRequest=storeRequest.read()
-    storeRequest.close()
-    print("read_storeRequest ",read_storeRequest)
-    read_storeRequest1=read_storeRequest.count('Request')
-    read_storeRequest2=read_storeRequest.count('Response')
-    print(read_storeRequest1," <> ",read_storeRequest)     
-    print("read_storeRequest ",read_storeRequest)     
-    print("________________________________________________________________exp ",read_storeRequest1," : ",read_storeRequest2)
-    if read_storeRequest1==read_storeRequest2:
-     return "<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'><meta http-equiv='refresh' content='0;URL=/CommUpdateExcel?'><html>This Message indicates an error in URL Forward</html>"
-    if read_storeRequest1!=read_storeRequest2:
-     return '<meta http-equiv="refresh" content="120"><html>LOADING..... This can Take up to 20 minuites</html>'
-
-
-
-@app.route('/CommUpdateExcel')
-def CommUpdateExcel():
- global domain
- domain=domain
- return render_template('/CommUpdateExcel.html',CacheBreakStamp=datetime.now(),domain=domain)
-
-
-@app.route('/GKW')
-def GoogleKWG():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleKeywords/GKW.xlsx",\
-                  attachment_filename="GKW.xlsx")
-
-@app.route('/GoogleKWSBMM')
-def GoogleKWSBMMKW():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleKeywords/GoogleBMMKW/DefaultSheet.xlsx",\
-                  attachment_filename="GoogleBMMKW.xlsx")
-         
-@app.route('/GoogleKWSB')
-def GoogleKWSBKW():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleKeywords/GoogleBroadKW/DefaultSheet.xlsx",\
-                  attachment_filename="GoogleKWSB.xlsx")
-
-@app.route('/GoogleKWSX')
-def GoogleKWSX():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleKeywords/GoogleExactKW/DefaultSheet.xlsx",\
-                  attachment_filename="GoogleKWSX.xlsx")
-
-
-
-
-@app.route('/GADA')
-def GADAG():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionA/GADA.xlsx",\
-                 attachment_filename="GADA.xlsx")
-@app.route('/GADB')
-def GADBG():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionB/GADB.xlsx",\
-                 attachment_filename="GADB.xlsx")
-
-
-
-@app.route('/GoogleAdSBMMA')
-def GoogleASBMM():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionA/GoogleAdsVersionABMM/DefaultSheet.xlsx",\
-                 attachment_filename="GoogleAdSBMMA.xlsx")
-                 
-         
-
-
-@app.route('/GoogleAdSBMMB')
-def GoogleBSBMM():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionB/GoogleAdsVersionBBMM/DefaultSheet.xlsx",\
-                 attachment_filename="GoogleAdSBMMB.xlsx")
-                 
-         
-  
-@app.route('/GoogleAdSBMA')
-def GoogleASBM():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionA/GoogleAdsVersionABroad/DefaultSheet.xlsx",\
-                 attachment_filename="GoogleAdSBMA.xlsx")
-                
-         
-
-
-
-        
-
-@app.route('/GoogleAdSBMB')
-def GoogleBSBM():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionB/GoogleAdsVersionBBroad/DefaultSheet.xlsx",\
-                 attachment_filename="GoogleAdSBMB.xlsx")
-                 
-
-
-
-@app.route('/GoogleAdSXA')
-def GoogleASX():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionA/GoogleAdsVersionAExact/DefaultSheet.xlsx",\
-                 attachment_filename="GoogleAdASX.xlsx")
-              
-         
-
-
-@app.route('/GoogleAdSXB')
-def GoogleBSX():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Google/GoogleOutputs/GoogleAds/GoogleAdsVersionB/GoogleAdsVersionBExact/DefaultSheet.xlsx",\
-                 attachment_filename="GoogleAdBSX.xlsx")
-  
- 
-@app.route('/BKW')
-def BKWB():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingKW/BKW.xlsx",\
-                  attachment_filename="BKW.xlsx")
-   
-        
-
-@app.route('/BingKWSBMM')
-def BingSBMMKW():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingKW/BingKWBMM/DefaultSheet.xlsx",\
-                  attachment_filename="BingBMMKW.xlsx")
- 
-         
-@app.route('/BingKWSB')
-def BingSBKW():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingKW/BingKWBroad/DefaultSheet.xlsx",\
-                  attachment_filename="BingSBKW.xlsx")
-
-
-
-@app.route('/BingKWSX')
-def BingSXKW():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingKW/BingKWExact/DefaultSheet.xlsx",\
-                  attachment_filename="BingSXKW.xlsx")
-         
-
-         
-@app.route('/BADA')
-def BAGAB():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsAtype/BADA.xlsx",\
-                 attachment_filename="BADA.xlsx")
-
-@app.route('/BADB')
-def BAGBB():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsBtype/BADB.xlsx",\
-                 attachment_filename="BADB.xlsx")
-        
-        
-        
-        
-@app.route('/BingAdSBMMA')
-def BingASBMM():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsAtype/BingAdsAtypeBMM/DefaultSheet.xlsx",\
-                 attachment_filename="BingAdSBMMA.xlsx")
-                
-
-
-@app.route('/BingAdSBMMB')
-def BingBSBMM():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsBtype/BingAdsBtypeBMM/DefaultSheet.xlsx",\
-                 attachment_filename="BingAdSBMMB.xlsx")
-              
- 
-         
-         
-@app.route('/BingAdSBA')
-def BingASB():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsAtype/BingAdsAtypeBroad/DefaultSheet.xlsx",\
-                 attachment_filename="BingAdSBA.xlsx")
-              
-         
-         
-  
-
-@app.route('/BingAdSBB')
-def BingBSB():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsBtype/BingAdsBtypeBroad/DefaultSheet.xlsx",\
-                 attachment_filename="BingAdSBB.xlsx")
-                      
-                  
- 
-
-
-
-@app.route('/BingAdSXA')
-def BingASX():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsAtype/BingAdsAtypeExact/DefaultSheet.xlsx",\
-                 attachment_filename="BingAdSXA.xlsx")
-                  
-
-
-@app.route('/BingAdSXB')
-def BingBSX():
- return send_file("/var/www/workPortal/Sheets/CommunityUpdates/Bing/BingOutputs/BingAds/BingAdsBtype/BingAdsBtypeExact/DefaultSheet.xlsx",\
-                 attachment_filename="BingAdSXB.xlsx")
 
 
 
@@ -457,24 +236,7 @@ print("14")
 
 
 
-"""
-@app.route('/CommunityUpdates')
-def CommunitiesUploads():
-    if chckbdxcred().find("NULL")==-1:
-        print(str(chckbdxcred()));
-        return str(chckbdxcred());
-    return render_template('CommunitiesForm.html')
 
-@app.route('/CommunityFileHander', methods=['POST','GET'])
-def CommunityFileHandling():
-    return fileHandler.CommListFileHandler()
-   
-
-@app.route('/account')
-def acc():
-    return render_template('account.html')
-    
-"""    
 
 print("15")
     
