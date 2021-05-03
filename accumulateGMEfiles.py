c834f38913c1c4f8e2fa1f1393c114e377a2b5a3
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-03 14:43:11 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -1,3 +1,5 @@
+
+
 from datetime import datetime
 
 
@@ -7,4 +9,5 @@ filedate=str(datetime.now().date())
 ndq=str('curl '+nasdaqAbbreviations+' -o nasdaqAbbreviations-'+filedate)
 print(ndq)
 os.system(ndq)
-
+print("accumilateGMEfiles ran")
+print("accumilateGMEfiles ran")
