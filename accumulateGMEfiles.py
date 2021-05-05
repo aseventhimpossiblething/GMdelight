9b6e0149197107669da1aeef78d24e59c111c7b4
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-05 12:58:29 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -80,7 +80,7 @@ print(str(os.listdir())
 
 
 
-chartiexdata="https://cloud.iexapis.com/stable/stock/XOM/chart/1m?token=Tpk_ae999384a70348b3855e8904d4c46e5e"
+chartiexdata="https://cloud.iexapis.com/stable/stock/XOM/chart/1m?token=pk_ae999384a70348b3855e8904d4c46e5e"
 
 
 #https://sandbox.iexapis.com/stable/stock/AMD/dividends/1y?token=Tpk_ae999384a70348b3855e8904d4c46e5e
@@ -88,5 +88,6 @@ chartiexdata="https://cloud.iexapis.com/stable/stock/XOM/chart/1m?token=Tpk_ae99
 #https://sandbox.iexapis.com/stable/stock/AMD/chart/1m?token=Tpk_ae999384a70348b3855e8904d4c46e5e
 expChart=requests.get(chartiexdata)
 print(expChart)
+print(expChart.json())
 
 
