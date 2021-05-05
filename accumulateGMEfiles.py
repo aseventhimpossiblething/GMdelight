5ea6c4f40e7595bcdfd019a71d677a7e24eadbb8
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-05 18:40:13 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -124,7 +124,7 @@ def columnMaker(columndata,columnNumber):
     print(len(x)); 
     count=0;
     for days in x:
-        print(" in new function "+days);
+        print(str(count)+" in new function "+str(days));
         print("----------------------") 
         print("----------------------")
         count=count+1;
