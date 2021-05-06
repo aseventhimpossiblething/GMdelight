975b845922a2fcbc622fd679264b7a6d571486b7
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-06 17:44:25 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -79,6 +79,7 @@ def runNasdaq():
             responseCode.append(rtest);
             print(str(rtest)+"   "+str(lineItem)+" of "+str(sze));
             lineItem=lineItem+1;
+        NasdaqTesFrame=pandas.DataFrame(NasdaqTesFrame,columns=['CQS Symbols']);    
         NasdaqTesFrame['broken line numbers']=brokenlines;
         NasdaqTesFrame['response Code']=responseCode;
         print(type(NasdaqTesFrame)) 
