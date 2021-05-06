acd0c0bdf9634a4605a9df1542efb7c199b46326
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-05 23:01:13 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -1,27 +1,19 @@
 
 """
- 
 Your dedicated access key is: 70YMNXM4BZWGEGOA
 Please record this API key at a safe place for future data access.
 https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMD&apikey=demo
-
 """
 # all IEX supported symbols https://cloud.iexapis.com/beta/ref-data/symbols?token=pk_2a5af8857a7940d4b361bc2b4a14d0adf
 # working example 1 quote current price https://cloud.iexapis.com/stable/stock/XOM/quote?token=pk_2a5af8857a7940d4b361bc2b4a14d0adf
 #zetapk_2a5af8857a7940d4b361bc2b4a14d0adf 
 #zetask_20d88bd4d61b4e92b2ae7b22d8f8f0aef
-#https://cloud.iexapis.com/stable/
-#https://cloud.iexapis.com/v1/
-#zeta/stock/{symbol}/batch
-#https://cloud.iexapis.com/stable/stock/aapl/batch?types=quote,news,chart&range=1m&last=10
-"""
-https://cloud.iexapis.com/stable/stock/aapl/batch?types=quote,news,chart&range=1m&last=10?token=pk_2a5af8857a7940d4b361bc2b4a14d0adf 
-https://cloud.iexapis.com/stable/stock/IBM/batch
-https://cloud.iexapis.com/stable/stock/market/batch?symbols=aapl,fb&types=quote,news,chart&range=1m&last=5
-https://cloud.iexapis.com/stable/stock/market/batch?symbols=aapl,fb&types=quote&range=1m&last=5
-#pip install iex-api-python 
-"""
- 
+
+#Below need testing
+#AlphaVantageEndPoint="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMD&apikey=70YMNXM4BZWGEGOA"
+#AlphaVantageAbbreviations="https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=70YMNXM4BZWGEGOA"
+#AlphaVantageEndPoint="https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbol=AMD&apikey=70YMNXM4BZWGEGOA"
+
 
 import requests
 import os
@@ -29,7 +21,7 @@ from datetime import datetime
 import pandas
 
 
-
+#Nasdaq Symbols
 def pullNasdaqAbbreves():
     os.chdir('Sheets/rememberGME/NasdaqAbbreviations');
     mglob=str(os.listdir());
@@ -54,21 +46,18 @@ def pullNasdaqAbbreves():
     
     print("accumilateGMEfiles ran")
     print("accumilateGMEfiles ran")
-"""    
-pullNasdaqAbbreves();
-NasdaqNativeAbbreviations=pandas.read_csv('ActiveNativendqAbbrev','|');
-NasdaqAbbreviations=pandas.read_csv('ActivendqAbbrev','|');
+    
+def runNasdaq():
+    pullNasdaqAbbreves();
+    NasdaqNativeAbbreviations=pandas.read_csv('ActiveNativendqAbbrev','|');
+    NasdaqAbbreviations=pandas.read_csv('ActivendqAbbrev','|');
 
-print(NasdaqAbbreviations)
-print(NasdaqNativeAbbreviations)
+    print(NasdaqAbbreviations);
+    print(NasdaqNativeAbbreviations);
+runNasdaq();
 
-"""
 
-#AlphaVantageEndPoint="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMD&apikey=70YMNXM4BZWGEGOA"
 
-#AlphaVantageAbbreviations="https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=70YMNXM4BZWGEGOA"
-
-#AlphaVantageEndPoint="https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbol=AMD&apikey=70YMNXM4BZWGEGOA"
 
 
 
