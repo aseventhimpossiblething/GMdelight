f2850e7bfab575dee77c3096e7dd0efd3bc7a77b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-06 00:21:47 -0500

    Update bdx-api-link.py

--- bdx-api-link.py
+++ bdx-api-link.py
@@ -68,7 +68,7 @@ def stonk():
     if chckbdxcred().find("NULL")==-1:
         print(str(chckbdxcred()));
         return str(chckbdxcred());
-    return str(accumulateGMEfiles.TableGen()[0]);
+    return str(accumulateGMEfiles.TableGen());
 
 
 
