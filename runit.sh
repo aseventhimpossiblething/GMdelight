c99127bda89771b4e6d959ca6abf32864948ea9c
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-17 19:09:04 -0500

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sleep 5
+cd /GMDelight/GMDelight
 #gunicorn -c appconfig.py bdx-api-link:app
-gunicorn --bind=127.0.0.1:5000 /GMDelight/GMDelight/bdx-api-link:app
+gunicorn --bind=127.0.0.1:5000 bdx-api-link:app
