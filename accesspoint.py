8a1487ca8fb1946caa5939c866b871e40d116aad
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-20 16:41:52 -0500

    Update and rename a to accesspoint.py

---
+++ accesspoint.py
@@ -0,0 +1,6 @@
+
+from argparse import ArgumentParser
+from tbselenium.tbdriver import TorBrowserDriver 
+from tbselenium.utils import start_xvfb, stop_xvfb
+from os.path import join, dirname, realpath
+
