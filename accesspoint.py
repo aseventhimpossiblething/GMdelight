75228ae985c88a3edf7db9bb41d2557a76472396
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-21 17:48:30 -0500

    Update accesspoint.py

--- accesspoint.py
+++ accesspoint.py
@@ -13,7 +13,7 @@ from tbselenium.tbdriver import TorBrowserDriver
 from os.path import join, dirname, realpath
 
 
-
+"""
 out_img = join(dirname(realpath(__file__)), "headless_screenshot.png")
 xvfb_display = start_xvfb()
 with TorBrowserDriver('/home/manivannan/pythonexamle/selenium_example/tor-browser_en-US') as driver:
@@ -22,4 +22,10 @@ with TorBrowserDriver('/home/manivannan/pythonexamle/selenium_example/tor-browse
     print("Screenshot is saved as %s" % out_img)
 
 stop_xvfb(xvfb_display)
+"""
+
+with TorBrowserDriver('/etc/tor') as driver:
+    driver.load_url("https://check.torproject.org")
+    driver.get_screenshot_as_file(out_img)
+    print("Screenshot is saved as %s" % out_img)
 
