9913782a6a33bdc495cde9f344df6e310826a72b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-22 15:28:21 -0500

    Update accesspoint.py

--- accesspoint.py
+++ accesspoint.py
@@ -20,7 +20,7 @@ from selenium import webdriver
 import geckodriver_autoinstaller
 
 
-geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
+#geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
                                      # and if it doesn't exist, download it automatically,
                                      # then add geckodriver to path
 """
