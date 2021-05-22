bc8a1a32748cfaeb7b2b580f0e7b87c659b2842f
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-22 12:51:28 -0500

    Update gecko.sh

--- gecko.sh
+++ gecko.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 INSTALL_DIR="/usr/local/bin"
+curl https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US&attribution_code=c291cmNlPXN1cHBvcnQubW96aWxsYS5vcmcmbWVkaXVtPXJlZmVycmFsJmNhbXBhaWduPShub3Qgc2V0KSZjb250ZW50PShub3Qgc2V0KSZleHBlcmltZW50PShub3Qgc2V0KSZ2YXJpYXRpb249KG5vdCBzZXQpJnVhPWVkZ2UmdmlzaXRfaWQ9MTY2NjMyMzEwNi4xNjIxNjk3NDU0&attribution_sig=adb49d216df299c4f451d07e7698f3ed14f83d4927e9fc3ea41fe9d026ce7aad
 
 #json=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
 #url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
 #url==$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
 #curl -s -L "$url" | tar -xz
-wget https://www.mozilla.org/en-US/firefox/download/thanks/
+
+#wget https://www.mozilla.org/en-US/firefox/download/thanks/
 #curl -s https://www.mozilla.org/en-US/firefox/download/thanks/
 #curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest 
 #chmod +x firefox
