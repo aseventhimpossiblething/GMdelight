8e2e2f3af06c6049efa7e19f9f353409a61cb9be
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-05-22 11:34:14 -0500

    Update gecko.sh

--- gecko.sh
+++ gecko.sh
@@ -6,11 +6,11 @@ INSTALL_DIR="/usr/local/bin"
 #url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
 #url==$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
 #curl -s -L "$url" | tar -xz
-curl https://www.mozilla.org/en-US/firefox/download/thanks/
-curl https://api.github.com/repos/mozilla/geckodriver/releases/latest 
-chmod +x firefox
-chmod +x geckodriver
-sudo mv firefox "$INSTALL_DIR"
+curl -s https://www.mozilla.org/en-US/firefox/download/thanks/
+curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest 
+#chmod +x firefox
+#chmod +x geckodriver
+#sudo mv firefox "$INSTALL_DIR"
 #sudo mv geckodriver "$INSTALL_DIR"
 #echo "installed geckodriver binary in $INSTALL_DIR"
 echo "gecko.sh run"
