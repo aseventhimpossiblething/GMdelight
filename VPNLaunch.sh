e9a8c089c9a699508e315c334e211130adbe24dd
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-06-17 15:03:14 -0500

    Update and rename VPNLaunch.expect to VPNLaunch.sh

---
+++ VPNLaunch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#spawn /etc/openvpn openvpn us-nyc-st005.prof.surfshark.com_tcp.ovpn
+#send "EsmfuxDcxAPcenqaLaS6hjMm\nztsd2vLLhm4e9bWSEGf24JqK\n"
+#expect EOF
+cd /etc/openvpn
+expect zb2
