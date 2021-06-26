9aff3e9d74a901271cf718f18659f7c54fc55a29
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-06-26 17:36:20 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -69,7 +69,7 @@ def runNasdaq():
     
     TopSymbols=NasdaqNativeAbbreviations["Symbol"];
     BottomSymbols=NasdaqAbbreviations["ACT Symbol"];
-    STKsymbols=TopSymbols.append(BottomSymbols)
+    STKsymbols=TopSymbols.append(BottomSymbols).reset_index();
     print(TopSymbols)
     print(BottomSymbols) 
     print(STKsymbols)
