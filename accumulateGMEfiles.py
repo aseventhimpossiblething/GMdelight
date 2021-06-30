648d05b32f665233bdc90eb54e3e2cb92e43b896
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-06-30 17:00:28 -0400

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -176,6 +176,8 @@ def runNasdaq():
         arr1=arr1.rename(columns=arr1.iloc[0])
         arr1=arr1.drop([0]);
         arr1=arr1.reset_index();
+        arr1["date1"]=arr1.label;
+        arr1.drop["label", axis=1];
         #print(arr[0]);
         print(arr1);
         return arr;  
