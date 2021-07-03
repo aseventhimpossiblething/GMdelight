fcac0b0918cb3d79fb8770ecc6740614d7a02a27
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-03 14:29:51 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -101,7 +101,7 @@ def IEXColmaker():
               
             return shiftCol;
         tester=metricshift(arr1,'close');
-        arr1.drop([len(metrichift)])
+        arr1.drop([len(metrishift)])
         return arr1;  
                
 
