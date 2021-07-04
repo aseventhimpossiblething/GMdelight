67f8b29bba7b26edb65502605f48eb203827cc5f
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-04 18:39:43 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -41,6 +41,7 @@ def SinglestockIEXdict(x,y):
         while count<len(x):
               push=x[count][y];
               arr.append(push); 
+              print(arr)  
               count=count+1;
         out=pandas.DataFrame(arr, columns=[y]);
         print("end cycle SinglestockIEXdict(x,y) end cycle")
