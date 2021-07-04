a6973fd75232a7b2fc6d773830e6ce773bfec1a8
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-04 13:11:55 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -89,7 +89,7 @@ def IEXColmaker():
         #print(x);
         #print(y);
         x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2);
-        regressor(x_train,y_train)
+        regressor.fit(x_train,y_train)
         return arr1;  
                
 
