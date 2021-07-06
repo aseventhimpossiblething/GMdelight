549522703476eefe47978bd51928595675d15c0e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-05 19:18:22 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -59,8 +59,8 @@ def IEXColmaker():
         iexpull=requests.get(URLPull);
         vixPull=requests.get(vixPull);
         xlfPull=requests.get(xlfPull);
-        print("vixPull")
-        print(vixPull)
+        #print("vixPull")
+        #print(vixPull)
         iexdata=json.loads(iexpull.text);
         vixdata=json.loads(vixPull.text);
         #print("xlfPull")
