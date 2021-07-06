bf7ebd0513649b8cb4506c66e2a5f2978193cd8e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-05 19:27:05 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -102,7 +102,8 @@ def IEXColmaker():
         xlfarr=subtable(xlfdata,keys,"xlf");
         
         def dframemaker(x):
-            Newarr=pandas.DataFrame(x).transpose().rename(columns=Newarr.iloc[0]); 
+            Newarr=pandas.DataFrame(x).transpose();
+            Newarr=Newarr.rename(columns=Newarr.iloc[0]); 
             Newarr=Newarr.drop([0]).reset_index().drop(["label","symbol","id","key","subkey"], axis=1);
             return Newarr;    
         arr1=dframemaker(arr);
