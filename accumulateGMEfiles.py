f1be9df91fcef5e04cc757a7b2e538ccab6a6011
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-22 21:11:20 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -162,7 +162,7 @@ def IEXColmaker():
             print("len(y) ",len(y))
             print("type(len(y))-- ",type(len(y))) 
             #print(x[(len(x))-1]);    
-            print("x len pos --",x[len(x)-1],"---",y[len(x-1)])
+            print("x len pos --",x[len(x)-1],"---",y[len(x)-1])
             while count < len(x):
                   #print("Open")      
                   #print(len(x),"---",count," ",x[count],y[count]) 
