1e321cebc4b616e6d112db3b346e056d4e0af4fa
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-25 15:22:48 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -146,7 +146,7 @@ def IEXColmaker():
             print("last row? ",y1[f:])
             if len(y1)>len(x1):
                #y1=y1.drop([len(dayshiftedclose)]); 
-               y1=y1.drop([len(x1)]);
+               y1=y1.drop([len(y1)]);
             print("len 2------------------------------",len(y1));    
             print(y1[f:])
                 
