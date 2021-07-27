b87d2bd2be83b0dbe489c8c778dd4e20c48f5e02
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-07-27 11:07:25 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -231,8 +231,9 @@ def IEXColmaker():
                         print("compare() ,looping");
                         print("because xdesignator=tdesignator ",xdesignator,"==",tdesignator,"--",xdesignator==tdesignator)   
                         if len(topcan)>len(x1):
-                           topcan=topcan.drop([len(topcan)-1]);     
-                           return topcan;
+                           #topcan=topcan.drop([len(topcan)-1]);  
+                           compare(x1,topcan,z); 
+                           #return topcan;
                         compare(x1,topcan,z);
                      #return topcan;   
                      
