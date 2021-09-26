8542799a4349374383650f0e071ec2cb536cf169
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-26 09:01:12 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -2,7 +2,7 @@ print("pre import sql")
 import sqlite3
 SQLite3conn=sqlite3.connect("TB1")
 cursor=SQLite3conn.cursor();
-cursor.execute("select*from TB11;")
+cursor.execute(".header on; select*from TB11;")
 p=cursor.fetchall();
 print(cursor)
 print(p)
