6efd957cea8d79e7724caca8814c47607a7276d0
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-26 13:58:32 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -1,16 +1,19 @@
 print("pre import sql")
 import sqlite3
-SQLcolsnames="select names from PRAGMA_TABLE_INFO('TB11')"
-SQLcolsnames="PRAGMA"
+import pandas
+#SQLcolsnames="select names from PRAGMA_TABLE_INFO('TB11')"
+#SQLcolsnames="PRAGMA"
 SQLite3conn=sqlite3.connect("TB1")
 cursor=SQLite3conn.cursor();
-a=cursor.execute(SQLcolsnames)
+#a=cursor.execute(SQLcolsnames)
 b=cursor.execute("select*from TB11;")
+ColNames=pandas.Dataframe(cursor.description)
+
 #p=cursor.fetchall();
 
 print(cursor)
 print(cursor.description)
-print(SQLcolsnames)
-print(a)
+#print(SQLcolsnames)
+#print(a)
 print(b)
 print("post connect")
