281d8785bc078de9c3676b658a28ad614ec3d016
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-26 14:21:35 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -1,18 +1,23 @@
 print("pre import sql")
 import sqlite3
 import pandas
-#SQLcolsnames="select names from PRAGMA_TABLE_INFO('TB11')"
-#SQLcolsnames="PRAGMA"
-SQLite3conn=sqlite3.connect("TB1")
+
+def MakeDailyTable(x):
+    print("Make Data Input")
+    print(x) 
+    
+
+SQLite3conn=sqlite3.connect("DailyBD")
 cursor=SQLite3conn.cursor();
-#a=cursor.execute(SQLcolsnames)
-b=cursor.execute("select*from TB11 limit 1;")
+cursor.execute("create table DailyTable()")
+
+b=cursor.execute("select*from DailyTable limit 1;")
 c=ColNames=pandas.DataFrame(cursor.description)
 c=c[[0]];
 
 a=cursor.fetchall();
 
-print(cursor)
+#print(cursor)
 #print(cursor.description)
 #print(SQLcolsnames)
 print(a)
