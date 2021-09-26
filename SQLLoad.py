ff114bfc932ef8e507c3c1c1ff5171fe3d69be6e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-26 08:51:00 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -1,6 +1,6 @@
 print("pre import sql")
 import sqlite3
-SQLite3conn=sqlite3.connect("BD1")
+SQLite3conn=sqlite3.connect("DB1")
 cursor=SQLite3conn.cursor();
 p=cursor.execute(".database;")
 print(cursor)
