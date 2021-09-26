a0cc9a9ad23289fb83e225fd3d688ae0cc799807
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-26 09:18:43 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -3,12 +3,13 @@ import sqlite3
 SQLcolsnames="select names from PRAGMA_TABLE_INFO('TB11')"
 SQLite3conn=sqlite3.connect("TB1")
 cursor=SQLite3conn.cursor();
-#cursor.execute(".header on;")
-cursor.execute("select*from TB11;")
-p=cursor.fetchall();
+a=cursor.execute(SQLcolsnames)
+b=cursor.execute("select*from TB11;")
+#p=cursor.fetchall();
 
 print(cursor)
 print(cursor.description)
 print(SQLcolsnames)
-print(p)
+print(a)
+print(b)
 print("post connect")
