3955c0f19c90ffc9b83bcdee9526fe5cf90e91d3
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-27 13:45:32 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -83,7 +83,8 @@ def MakeDailyTable(z,a):
     cursor.execute(query);
     results=cursor.fetchall();
     results=pandas.DataFrame(results);
-    results=results.columns[novelXstr3];
+    print(results.description)
+    #results=results.columns[novelXstr3];
     print("results",results)
     print("novelXstr",novelXstr)
     print("novelXstr2",novelXstr2)
