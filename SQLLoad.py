8ab426f9a9ca498d758c7c447bc6be1a614a7f53
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-26 19:00:45 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -52,7 +52,7 @@ def MakeDailyTable(x):
     SQLite3conn=sqlite3.connect("DailyDB")
     cursor=SQLite3conn.cursor();
     cursor.execute(novelXstr2);
-    #cursor.executemany();
+    #cursor.executemany(novelXstr3,);
     cursor.execute(query);
     
     results=cursor.fetchall();
