66c39d509e2df29eb88a69e64bbde8132cad5cbf
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-27 22:16:37 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -460,7 +460,7 @@ DailyBasisInserter();
 #pullNasdaqAbbreves();
 #print(runNasdaq());
 #print(runNasdaq()['Symbols']);
-#IEXColmaker("AMD");
+IEXColmaker("AMD");
 #SqlCall=SQLLoad.CallFromSQL("*","DailyTable");
 #print(SqlCall);
 
