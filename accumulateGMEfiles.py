d2dac3d6a354431bcfe16d9f49366275a2fc6111
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-28 12:16:05 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -193,6 +193,9 @@ def IEXColmaker(TargetSymbol):
         print("Creation of xlfarrvix-------------------")
         print(xlfarrvix)
         
+        LastChartDate=xlfarrvix['date'][len(xlfarrvix['date']);
+        print("LastChartDate ",LastChartDate);                                
+        
         px=xlfarrvix.drop(['dayshiftedclose']);
         py=xlfarrvix['dayshiftedclose'];
         xTreeMod200=RandomForestRegressor(n_estimators = 200).fit(px,py);
