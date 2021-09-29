0825fd1e15585e1a190f30ab92aafecb31b9b92a
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-29 09:13:33 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -12,7 +12,7 @@ def CallFromSQL(x,y,date):
     date=str(date);
     #date2=str(date2);
     SQLite3conn=sqlite3.connect("DailyDB");
-    query="select*from DailyTable"
+    query="select * from DailyTable"
     query.replace("*",x).replace("DailyTable",y)
     if date.find("-")>-1:
        query=query+" where insertionDay = '"+date+"';"  
