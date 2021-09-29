a07df0a175b81522e9bdcbaa0ce204b3503bb553
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-29 08:46:39 -0500

    Update SQLLoad.py

--- SQLLoad.py
+++ SQLLoad.py
@@ -37,13 +37,14 @@ def CallFromSQL(x,y,date):
     return results;
 
 def MakeDailyTable(z,a):
-    """
+    
     SQLite3conn=sqlite3.connect("DailyDB")
     cursor=SQLite3conn.cursor();
     cursor.execute("drop table DailyTable;")
     SQLite3conn.commit()
     SQLite3conn.close()
-    """
+    SQLite3conn.commit()
+    
     
     tablename="DailyTable";
     
@@ -133,6 +134,7 @@ def MakeDailyTable(z,a):
           cursor.execute(insertionOrder);
           SQLite3conn.commit() 
           SQLite3conn.close();
+          SQLite3conn.commit()
           rowcount=rowcount+1;
     
     """
