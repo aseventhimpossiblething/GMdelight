38cf1e4b261457005744a8bd5f6b55b03900546a
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-09-29 13:09:13 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -205,7 +205,7 @@ def IEXColmaker(TargetSymbol):
         LastChartRow=LastChartRow.drop(['dayshiftedclose','date'], axis=1);
         
         #print("LastChartRow ",LastChartRow);
-        px=xlfarrvix.drop(['dayshiftedclose','date','Symbol','insertionDay'], axis=1);
+        px=xlfarrvix.drop(['dayshiftedclose','date','Symbols','insertionDay'], axis=1);
         py=xlfarrvix['dayshiftedclose'];
         print(px.columns)
         print(px)
