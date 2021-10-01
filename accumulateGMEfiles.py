cde4f3ebcb376b48bc7612a6acef5d34f82cf168
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-10-01 12:58:15 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -59,7 +59,7 @@ def projection(xlfarrvix):
        
         LastChartRow=xlfarrvix.iloc[len(xlfarrvix['date'])-2:];
         print("type(LastChartRow.columns) ---- ",type(LastChartRow.columns))
-        LCR=LastChartRow.columns.str
+        LCR=LastChartRow.columns.values.to_string()
         print("LCR----------------LCR-----",LCR)
         print("LCR----------------LCR-----find Symbol---",LCR.find('Symbol'))
         print("LCR----------------LCR-----find Symbol---",LCR.find('Symbol'))
