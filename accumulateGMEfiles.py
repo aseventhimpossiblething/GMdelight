8e386cff56b30de150725b90004a2d1170a9f94b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-10-03 09:32:34 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -122,7 +122,7 @@ def projection(xlfarrvix):
             #print(x[[newtitle][0]])
                 
             count=0;
-            while count <= len(newcols[0]-1):
+            while count <= len(newcols):
                   print("top ------- ",count);
                   print("len(newcols[0] ",len(newcols[0])); 
                   print("len(newcols[0]-1 ",len(newcols[0]-1));
