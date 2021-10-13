7e7aae903ce5bccb813976c1b4cd24394aebd88b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-10-12 20:01:01 -0500

    Update accumulateGMEfiles.py

--- accumulateGMEfiles.py
+++ accumulateGMEfiles.py
@@ -157,6 +157,8 @@ def projection(XAV):
                         
                   if corrElement<mean:
                      corIndex=corrlist.index(corrElement);
+                     cnum=corIndex.find('dayshiftedclose');
+                     print("cnum -- 161 ",cnum);   
                      print("corIndex in if loop -- 156 ",corIndex);
                      print("corrElement in if loop -- 156 ",corrElement);
                      print("x[dayshiftedclose]  in if loop -- 156 ",x['dayshiftedclose']);
